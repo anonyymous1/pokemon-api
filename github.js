0c219288733912ae0e4c098d77fa80cc4e0db930
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-10-28 11:36:41 -0400

    [github.js]Add new object and element to page.

--- github.js
+++ github.js
@@ -1,10 +1,33 @@
+const container = document.querySelector('.container');
+
+
 fetch('https://api.github.com/users/anonyymous')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const rubenObj = {
+        bio: githubData.bio, // grabs the bio key from the github data
+        username: githubData.login,
+        name: githubData.name
+    }
+    console.log(rubenObj);
+
+    const newElement = document.createElement('p');  // this makes a new element <p></p>
+        newElement.textContent = rubenObj.name;     //  and sets the value to the name
+        container.appendChild(newElement);         //   and appends that to the parent container
 })
 .catch(error => {
     console.log(error);
 })
+
+// fetch('https://api.github.com/users/anonyymous')
+// .then(response => {
+//     return response.json();
+// })
+// .then(githubData => {
+//     console.log(githubData);
+// })
+// .catch(error => {
+//     console.log(error);
+// })
